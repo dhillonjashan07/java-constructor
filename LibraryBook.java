a27d971629ad581b87e7e2ced8519a71aa343931
jashan dhillon <jashandhillon595@gmail.com>
2025-03-10 15:28:04 +0530

    [jashan] Add:librarybook

---
+++ LibraryBook.java
@@ -0,0 +1,31 @@
+public class LibraryBook {
+    private String title;
+    private String author;
+    private double price;
+    private boolean availability;
+
+    // Default constructor
+    public LibraryBook() {
+        this.title = "Unknown Title";
+        this.author = "Unknown Author";
+        this.price = 0.0;
+        this.availability = true;
+    }
+
+    // Parameterized constructor
+    public LibraryBook(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.availability = true;
+    }
+
+    public String borrow() {
+        if (availability) {
+            availability = false;
+            return "You have borrowed '" + title + "'.";
+        } else {
+            return "'" + title + "' is currently unavailable.";
+        }
+    }
+}
